b487e9551393de453fa3a62d483bee1262da0c9f
JamesUdy-Terra <udhayaprakash@letsterra.com>
2025-06-27 12:48:46 +0530

    refactor: change the `port` to listen

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ async def swap_face_api(source_image: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
