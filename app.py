f1630cbb5117b764f1299d87ce1750cbf0448f3e
JamesUdy-Terra <udhayaprakash@letsterra.com>
2025-06-30 16:41:07 +0530

    refactor: modify `face-swap` post api (v2)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
 from fastapi.responses import Response
 from roop.core import (
     start,
